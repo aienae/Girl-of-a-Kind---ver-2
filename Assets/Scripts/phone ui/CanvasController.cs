f54debacabc38675d2009fc6f10ccc9e56899490
aienae <inayah.iq@gmail.com>
2024-04-06 14:16:50 +0100

    shop and inventory done

--- Assets/Scripts/phone ui/CanvasController.cs
+++ Assets/Scripts/phone ui/CanvasController.cs
@@ -7,25 +7,45 @@ public class CanvasController : MonoBehaviour
 {
     public GameObject childMainMenu;
     public GameObject childShopMenu;
+    public GameObject childInvenMenu;
 
     public Button shoppingButton;
     public Button shopBackButton;
 
+    public Button InventoryButton;
+    public Button InvenBackButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //childInvenMenu.SetActive(false);
+        //childShopMenu.SetActive(false);
+        childMainMenu.SetActive(true);
     }
 
     public void ShopButtonClicked()
     {
         childMainMenu.SetActive(false);
         childShopMenu.SetActive(true);
-
+        childInvenMenu.SetActive(false);
     }
     public void ShopBackClicked()
     {
         childShopMenu.SetActive(false);
+        childMainMenu.SetActive(true);
+
+    }
+
+    public void InventoryButtonClicked()
+    {
+        childMainMenu.SetActive(false);
+        childInvenMenu.SetActive(true);
+        childShopMenu.SetActive(false);
+
+    }
+        public void InvenBackClicked()
+    {
+        childInvenMenu.SetActive(false);
         childMainMenu.SetActive(true);
 
     }
--- Assets/Scripts/phone ui/shop_UI.cs
+++ Assets/Scripts/phone ui/shop_UI.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class shop_UI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Shopitem
     {
-        
+        public string itemName;
+        public int itemCost;
+        public int inventoryCount;
     }
 
-    // Update is called once per frame
-    void Update()
+    public TMP_Text displayBalance;
+    public int currentBalance = 500;
+
+    public GameObject itemButton1;
+    public GameObject itemButton2;
+    public TMP_Text[] inventoryTexts;
+    public Shopitem[] items;
+
+    
+    void Start()
+    {
+       UpdateBalanceDisplay();
+       UpdateInventoryDisplay();
+    }
+
+
+    public void BuyItemClicked(int itemIndex)
     {
         
+        if (currentBalance >= items[itemIndex].itemCost)
+        {
+            currentBalance -= items[itemIndex].itemCost;
+            items[itemIndex].inventoryCount++;
+            UpdateBalanceDisplay();
+            UpdateInventoryDisplay();
+        }
+        else
+        {
+            Debug.Log("not enough money to make purchase!");
+        }
+    }
+
+    public void UpdateBalanceDisplay()
+    {
+        displayBalance.text = "Balance: $" + currentBalance.ToString();
+    }
+
+    public void UpdateInventoryDisplay()
+    {
+       for (int i = 0; i < items.Length; i++)
+        {
+            inventoryTexts[i].text = items[i].inventoryCount.ToString();
+        }
     }
 }
